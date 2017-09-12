e45adac273b036f038216cfb4cdb3e26e5598f09
yurialeksndr <yuriatp@gmail.com>
2017-09-11 22:48:32 -0300

    Cria a estrutura básica de código do 'bluetooth controlled robot'.

--- bluetooth-controlled-robot/code/bluetooth-controlled-robot/bluetooth-controlled-robot.ino
+++ bluetooth-controlled-robot/code/bluetooth-controlled-robot/bluetooth-controlled-robot.ino
@@ -1,4 +1,9 @@
-nt btValue;
+#include <AFMotor.h>
+
+int btValue;
+
+AF_DCMotor motor_d(1);
+AF_DCMotor motor_e(2);
 
 void setup() {
   
@@ -10,25 +15,65 @@ void loop() {
   
   if(Serial.available()) {
     btValue = Serial.read();
-    Serial.println(btValue);
+    //Serial.println(btValue);
     
     if (btValue == 'a') {
+        motor_d.setSpeed(255);
+        motor_e.setSpeed(255);
+        motor_d.run(FORWARD);
+        motor_e.run(FORWARD);
 
+        delay(300);
+
+        motor_d.setSpeed(0);
+        motor_e.setSpeed(0);
+        motor_e.run(RELEASE);
+        motor_d.run(RELEASE);
     }
     
     else if (btValue == 'b') {
-      
+        motor_d.setSpeed(255);
+        motor_e.setSpeed(255);
+        motor_d.run(BACKWARD);
+        motor_e.run(BACKWARD);
+
+        delay(300);
+
+        motor_d.setSpeed(0);
+        motor_e.setSpeed(0);
+        motor_e.run(RELEASE);
+        motor_d.run(RELEASE);
     }
 
     else if (btValue == 'c') {
-      
+        motor_d.setSpeed(255);
+        motor_e.setSpeed(255);
+        motor_d.run(FORWARD);
+        motor_e.run(BACKWARD);
+
+        delay(300);
+
+        motor_d.setSpeed(0);
+        motor_e.setSpeed(0);
+        motor_e.run(RELEASE);
+        motor_d.run(RELEASE);
     }
 
     else if (btValue == 'd') {
-      
+        motor_d.setSpeed(255);
+        motor_e.setSpeed(255);
+        motor_d.run(BACKWARD);
+        motor_e.run(FORWARD);
+
+        delay(300);
+
+        motor_d.setSpeed(0);
+        motor_e.setSpeed(0);
+        motor_e.run(RELEASE);
+        motor_d.run(RELEASE);
     }
 
-    delay(30);
+    delay(50);
 
   }
 }
