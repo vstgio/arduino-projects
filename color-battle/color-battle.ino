eb9b8865f78ac4c991c36bba04aee848ae890dc5
yurialeksndr <yuriatp@gmail.com>
2018-08-15 17:11:15 -0300

    Color Battle: Implementa sistema de pontos (Processing) e de verificação se o usuário está segurando o botão para tentar ganhar.

--- color-battle/color-battle.ino
+++ color-battle/color-battle.ino
@@ -1,3 +1,8 @@
+int player01_state = 0;
+int player02_state = 0;
+int player01_last_state = 0;
+int player02_last_state = 0;
+
 void setup() {
   Serial.begin(9600);
   pinMode(2, INPUT);
@@ -5,17 +10,53 @@ void setup() {
 }
 
 void loop() {
-  if (digitalRead(2) == true && digitalRead(3) == true){
+  if (digitalRead(2) != player01_last_state) {
+    if (digitalRead(2) == true) {
+      player01_last_state = true;
+      player01_state = true;
+    }
+    
+    else {
+      player01_last_state = false;
+      player01_state = false;
+    }
+  }
+  
+  else {
+      player01_state = false;
+  }
+
+  if (digitalRead(3) != player02_last_state) {
+    if (digitalRead(3) == true) {
+      player02_last_state = true;
+      player02_state = true;
+    }
+  
+    else {
+      player02_last_state = false;
+      player02_state = false;
+    }
+  }
+  
+  else {
+      player02_state = false;
+  }
+  
+  sendData(player01_state, player02_state);
+}
+
+void sendData(int player01, int player02) {
+  if (player01 == true && player02 == true) {
     Serial.println("1,1");
   }
-
-  else if (digitalRead(2) == true && digitalRead(3) == false) {
+  
+  else if (player01 == true && player02 == false) {
     Serial.println("1,0");
   }
-
-  else if (digitalRead(2) == false && digitalRead(3) == true) {
+  
+  else if (player01 == false && player02 == true) {
     Serial.println("0,1");
   }
   
-  delay(20);
+  delay(20);  
 }
--- color-battle/color_battle/color_battle.pde
+++ color-battle/color_battle/color_battle.pde
@@ -4,16 +4,17 @@ Serial port;
 String arduinovalue;
 String[] separatedvalues;
 int score;
+int proportion = 40;
 int newlinecode = 10;
 
 void setup() {
   fullScreen();
-  score = width/2;
+  score = 20;
   
   noStroke();
   background(89, 171, 227);
   fill(242, 121, 53);
-  rect(0, 0, score, height);
+  rect(0, 0, score*(width/proportion), height);
   
   port = new Serial(this, Serial.list()[Serial.list().length-1], 9600);
   port.clear();
@@ -35,7 +36,7 @@ void draw() {
       else if (int(separatedvalues[0]) == 0 && int(separatedvalues[1]) == 1) {
         score = score - 1;
       }
-      rect(0, 0, score, height);
+      rect(0, 0, score*(width/proportion), height);
     }
   }
 }
